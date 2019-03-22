79162cd4dc3b58c3306a7afa74beb142022f7440
isaiah352 <dix.isaiah@gmail.com>
2019-03-22 02:06:13 -0500

    initial commit

---
+++ src/main/java/com/movies/demo/MoviesApplication.java
@@ -0,0 +1,14 @@
+package com.movies.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/movies/demo/model/Movie.java
@@ -0,0 +1,42 @@
+package com.movies.demo.model;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+    private String name;
+    private String genre;
+    private String year;
+    private String rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
